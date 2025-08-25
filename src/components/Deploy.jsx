d5d5a34f03a17ec1bf0d7ec1f820f53c1794ec13
nafij-islam <sahariannafis70@gmail.com>
2025-08-25 10:22:52 +0600

    update

--- src/components/Deploy.jsx
+++ src/components/Deploy.jsx
@@ -6,15 +6,15 @@ const Deploy = () => {
   return (
     <section className="py-[50px]">
       <Container>
-        <Row>
-          <Col lg={4} md={6}>
-            <div className="flex items-center mb-[20px] gap-[50px] hover:shadow-md hover:shadow-red-300 duration-300 bg-[#fad0bc8f] pt-[30px] pb-[20px] pr-[40px] pl-[25px] rounded-[15px]">
+        <Row >
+          <Col lg={4} md={6} sm={12}>
+            <div className="flex card-main items-center mb-[20px] gap-[50px] hover:shadow-md hover:shadow-red-300 duration-300 bg-[#fad0bc8f] pt-[30px] pb-[20px] pr-[40px] pl-[25px] rounded-[15px]">
               <div className=" after:absolute after:border-[1px] after:border-[#fad0bcf1]  after:content-[''] relative after:top-[-23px] after:right-[-20px] after:h-[70px] ">
                 <img src={ball} />
               </div>
               <div>
                 <h2 className="pb-[7px] text-[20px]">Immediate Deployment</h2>
-                <p className="w-[200px]">
+                <p className="w-[200px] diploy-pera" >
                   Et vero eos et accusamus et iusto odio dignissimos
                 </p>
               </div>
@@ -28,21 +28,21 @@ const Deploy = () => {
               </div>
               <div>
                 <h2 className="pb-[7px] text-[20px]">Immediate Deployment</h2>
-                <p className="w-[200px]">
+                <p className="w-[200px] diploy-pera">
                   Et vero eos et accusamus et iusto odio dignissimos
                 </p>
               </div>
             </div>
           </Col>
 
-          <Col lg={4}>
+          <Col lg={4} md={6}>
             <div className="flex items-center gap-[50px] hover:shadow-md hover:shadow-red-300 duration-300 bg-[#fad0bc8f] pt-[30px] pb-[20px] pr-[40px] pl-[25px] rounded-[15px]">
               <div className=" after:absolute after:border-[1px] after:border-[#fad0bcf1]  after:content-[''] relative after:top-[-23px] after:right-[-20px] after:h-[70px] ">
                 <img src={ball} />
               </div>
               <div>
                 <h2 className="pb-[7px] text-[20px]">Immediate Deployment</h2>
-                <p className="w-[200px]">
+                <p className="w-[200px] diploy-pera">
                   Et vero eos et accusamus et iusto odio dignissimos
                 </p>
               </div>
